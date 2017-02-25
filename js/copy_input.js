ba7246e396b2757de6874f85596692c62e4f724a
Neil Lawrence <lawrennd@gmail.com>
2017-02-25 19:14:24 +0000

    Update copy me area.

--- js/copy_input.js
+++ js/copy_input.js
@@ -1,50 +1,16 @@
-var copyBtn   = $("#copy-btn"),
-input     = $("#copy-me");
 
-function copyToClipboardFF(text) {
-  window.prompt ("Copy to clipboard: Ctrl C, Enter", text);
+function CopyToClipboard(containerid) {
+if (document.selection) { 
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("Copy"); 
+
+} else if (window.getSelection) {
+    var range = document.createRange();
+     range.selectNode(document.getElementById(containerid));
+     window.getSelection().addRange(range);
+     document.execCommand("Copy");
+     alert("text copied") 
 }
-
-function copyToClipboard() {
-  var success   = true,
-      range     = document.createRange(),
-      selection;
-
-  // For IE.
-  if (window.clipboardData) {
-    window.clipboardData.setData("Text", input.val());        
-  } else {
-    // Create a temporary element off screen.
-    var tmpElem = $('<div>');
-    tmpElem.css({
-      position: "absolute",
-      left:     "-1000px",
-      top:      "-1000px",
-    });
-    // Add the input value to the temp element.
-    tmpElem.text(input.val());
-    $("body").append(tmpElem);
-    // Select temp element.
-    range.selectNodeContents(tmpElem.get(0));
-    selection = window.getSelection ();
-    selection.removeAllRanges ();
-    selection.addRange (range);
-    // Lets copy.
-    try { 
-      success = document.execCommand ("copy", false, null);
-    }
-    catch (e) {
-      copyToClipboardFF(input.val());
-    }
-    if (success) {
-      alert ("The text is on the clipboard, try to paste it!");
-      // remove temp element.
-      tmpElem.remove();
-    } else {
-      alert ("It didn't work");
-      tmpElem.remove();
-    }
-  }
 }
-
-copyBtn.on('click', copyToClipboard);
