28fa183718295090fc3b42db77124d3d0374bf99
Neil Lawrence <lawrennd@gmail.com>
2017-02-25 19:46:32 +0000

    Update codebox type

--- js/copy_input.js
+++ js/copy_input.js
@@ -3,12 +3,14 @@ if (document.selection) {
     var range = document.body.createTextRange();
     range.moveToElementText(document.getElementById(containerid));
     range.select().createTextRange();
-    document.execCommand("Copy"); 
+    document.execCommand("Copy");
+    document.selection.empty();
     
 } else if (window.getSelection) {
     var range = document.createRange();
     range.selectNode(document.getElementById(containerid));
     window.getSelection().addRange(range);
     document.execCommand("Copy");
+    window.getSelection().removeAllRanges();
 }
 }
