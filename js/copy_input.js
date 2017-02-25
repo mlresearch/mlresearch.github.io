63430b6b7f2d4e0c392d016326ec2600cb85d1d1
Neil Lawrence <lawrennd@gmail.com>
2017-02-25 19:25:22 +0000

    Update copy me area.

--- js/copy_input.js
+++ js/copy_input.js
@@ -1,15 +1,14 @@
-
 function CopyToClipboard(containerid) {
 if (document.selection) { 
     var range = document.body.createTextRange();
     range.moveToElementText(document.getElementById(containerid));
     range.select().createTextRange();
     document.execCommand("Copy"); 
-
+    
 } else if (window.getSelection) {
     var range = document.createRange();
-     range.selectNode(document.getElementById(containerid));
-     window.getSelection().addRange(range);
-     document.execCommand("Copy");
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("Copy");
 }
 }
