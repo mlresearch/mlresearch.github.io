c9dd0f3dee1051d5890e8d900eed2cdf431332ae
Neil Lawrence <lawrennd@gmail.com>
2017-03-11 18:05:36 +0000

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 <!-- solution from http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file -->
-function download(filename, text) {
+function DownloadToFile(filename, containerid) {
     var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(document.getElementById(containerid).innerHTML));
     pom.setAttribute('download', filename);
 
     if (document.createEvent) {
